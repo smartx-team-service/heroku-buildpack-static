7f13ba78f2f704509aade5e5cdaf4ff33f579f37
smartx-team-service <128409865+smartx-team-service@users.noreply.github.com>
2023-04-28 13:12:05 -0400

    Update nginx.conf.erb, added "host_for_sendgrid"

--- scripts/config/lib/nginx_config.rb
+++ scripts/config/lib/nginx_config.rb
@@ -26,6 +26,8 @@ class NginxConfig
     json["port"] ||= ENV["PORT"] || 5000
     json["root"] ||= DEFAULT[:root]
     json["encoding"] ||= DEFAULT[:encoding]
+    
+    json["host_for_sendgrid"] = ENV["HOST_FOR_SENDGRID"]
 
     json["canonical_host"] ||= DEFAULT[:canonical_host]
     json["canonical_host"] = NginxConfigUtil.interpolate(json["canonical_host"], ENV) if json["canonical_host"]
